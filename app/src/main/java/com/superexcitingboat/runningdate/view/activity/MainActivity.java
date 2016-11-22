d1559235a28c0885db31f836dbddad36aca0dacc
SuperBigHead <391830728@qq.com>
2016-11-22 14:47:37 +0800

    完成了底部导航栏的功能

--- app/src/main/java/com/superexcitingboat/runningdate/view/activity/MainActivity.java
+++ app/src/main/java/com/superexcitingboat/runningdate/view/activity/MainActivity.java
@@ -2,14 +2,28 @@ package com.superexcitingboat.runningdate.view.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.superexcitingboat.runningdate.R;
+import com.superexcitingboat.runningdate.view.fragment.bottom.PersonalFragment;
+import com.superexcitingboat.runningdate.view.fragment.bottom.RankingFragment;
+import com.superexcitingboat.runningdate.view.fragment.bottom.SportsFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    public static final String TAG = "MainActivityTAG";
+    FragmentManager mFragmentManager;
+    private List<Fragment> mFragments = new ArrayList<>();
+    RadioGroup mRadioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +43,54 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.bt_location).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,LocationActivity.class));
+                startActivity(new Intent(MainActivity.this, LocationActivity.class));
             }
         });
+
+        initFragments();
+        initView();
+    }
+
+    private void initFragments() {
+        mFragments.add(new SportsFragment());
+        mFragments.add(new RankingFragment());
+        mFragments.add(new PersonalFragment());
+    }
+
+    private void initView() {
+        mFragmentManager = getSupportFragmentManager();
+
+        //设置默认显示主页这个fragment
+        FragmentTransaction transaction1 = mFragmentManager.beginTransaction();
+        transaction1.replace(R.id.content, mFragments.get(0));
+        transaction1.commit();
+
+        mRadioGroup = (RadioGroup) findViewById(R.id.bottom_rg);
+        mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i){
+                    case R.id.bottom_sports:
+                        FragmentTransaction transaction1 = mFragmentManager.beginTransaction();
+                        transaction1.replace(R.id.content, mFragments.get(0));
+                        transaction1.commit();
+                        break;
+                    case R.id.bottom_ranking:
+                        FragmentTransaction transaction2 = mFragmentManager.beginTransaction();
+                        transaction2.replace(R.id.content, mFragments.get(1));
+                        transaction2.commit();
+                        break;
+                    case R.id.bottom_personal:
+                        FragmentTransaction transaction3 = mFragmentManager.beginTransaction();
+                        transaction3.replace(R.id.content, mFragments.get(2));
+                        transaction3.commit();
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/superexcitingboat/runningdate/view/fragment/bottom/SportsFragment.java
@@ -0,0 +1,25 @@
+package com.superexcitingboat.runningdate.view.fragment.bottom;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.superexcitingboat.runningdate.R;
+
+
+/**
+ * Created by xushuzhan on 2016/11/21.
+ */
+
+public class SportsFragment extends Fragment {
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_sports,container,false);
+        return view;
+    }
+}
